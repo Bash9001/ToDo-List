af1773f91f0036a2b86f319fe4d8d47e65ac2ba2
Mubashir Ali <mubashir@DebianTower.SUPAHOT500.net>
2023-02-06 21:36:45 +0000

    Initial code commit

---
+++ src/TaskFormActions.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById('taskFormContainer').style.display = 'block'
+}
+
+function closeForm() {
+    document.getElementById('taskFormContainer').style.display = 'none'
+}
